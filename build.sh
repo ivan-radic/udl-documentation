5f93cc109b35a952a227d966c93b81695775c2e1
Edditoria <edditoria@gmail.com>
2020-06-20 13:26:33 +0800

    Update build.sh file; Run it to build all bootstrap files

--- build.sh
+++ build.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 
-bootstrap_path=$(bundle show bootstrap)
+bootstrap_path=$(bundle info bootstrap --path)
 
+# make directory if not exist
 mkdir -p _sass
+
+# remove existing bootstrap files
+rm -rf _sass/bootstrap
+rm -rf _sass/_bootstrap-grid.scss
+rm -rf _sass/_bootstrap-reboot.scss
+rm -rf _sass/_bootstrap.scss
+
 cp -R $bootstrap_path/assets/stylesheets/ _sass
